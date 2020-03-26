1d0fbecae7c9b2de2d7bef049a789c3b37c980b8
Hy Khang <tranhykhang8108@gmail.com>
2020-03-26 20:33:29 +0700

    set-timeout

---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * Tạo 1 hàm doAfter nhận vào 2 tham số:
+ *  - Tham số thứ 1: 1 function
+ *  - Tham số thứ 2: Thời gian x (ms)
+ * Hàm này sẽ gọi function sau 1 khoảng thời gian x ms
+ */
+
+//  function doAfter(func, x) {
+//  	setTimeout(func, x);
+//  }
+
+// function done() {	
+// 	console.log('hy khang dep trai');
+// }
+
+// console.log('start');
+// doAfter(done, 1);
+// console.log('done');
+
+//=================================================================
+/**
+ * Tạo 1 hàm doAfter nhận vào 2 tham số:
+ *  - Tham số thứ 1: 1 function
+ *  - Tham số thứ 2: Thời gian x (ms)
+ * Hàm này sẽ gọi function sau 1 khoảng thời gian x ms VÀ trả về 1 promise để có thể gọi như sau
+ */
+function doAfter(func, x) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      
+      resolve(func());
+    }, x);
+  });
+}
+
+function sayHello() {
+  console.log('Hello');
+}
+
+function sayGoodbye() {
+  console.log('Goodbye');
+}
+
+doAfter(sayHello, 1000).then(sayGoodbye);
+// Expect: 
+// Đợi 1s
+// Hello
+// Goodbye
